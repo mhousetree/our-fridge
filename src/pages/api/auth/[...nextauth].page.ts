7e53dca89878e4ad003afab087889cf58a6e6499
mhousetree <74708840+mhousetree@users.noreply.github.com>
2024-01-19 21:56:04 +0900

    use next-auth to use twitter authentication

--- src/pages/api/auth/[...nextauth].page.ts
+++ src/pages/api/auth/[...nextauth].page.ts
@@ -1,10 +1,26 @@
+import { FirestoreAdapter } from '@next-auth/firebase-adapter';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import TwitterProvider from 'next-auth/providers/twitter';
+import { credential } from '../utils/firebaseHelper';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: FirestoreAdapter({
+    credential,
+  }),
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      session.user = {
+        id: user.id,
+      };
+      return session;
+    },
+  },
 };
+
+export default NextAuth(authOptions);
